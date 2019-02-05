797d291660f8ae412bafa8dfe4b4191ffc3da218
Irina Nita <irinanita.im@gmail.com>
2019-02-05 17:17:07 +0000

    Wired form to the backend/insert recipe into DB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -8,6 +8,7 @@ app.config["MONGO_DBNAME"]="cook_book"
 app.config["MONGO_URI"]="mongodb://admin:ceckbrb05@ds121295.mlab.com:21295/cook_book"
 
 mongo=PyMongo(app)
+
 
 @app.route('/')
 def index():
@@ -23,6 +24,33 @@ def add_recipe():
     return render_template('add_recipe.html',_allergens=allergens_list,
     _diet=diet_list,_cuisine=cuisine_list,units=units_list)
 
+@app.route('/insert_recipe', methods =["POST","GET"])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes_dict=request.form.to_dict()
+    headers = ('name', 'qty','units')
+    values = (
+        request.form.getlist('ingredient-name[]'),
+        request.form.getlist('ingredient-qty[]'),
+        request.form.getlist('ingredient-units[]'),
+    )
+    items = [{} for i in range(len(values[0]))]
+    for x,i in enumerate(values):
+        for _x,_i in enumerate(i):
+            items[_x][headers[x]] = _i
+    
+    form_allergens = request.form.getlist("allergens")
+    form_steps = request.form.getlist("steps")
+    print(form_steps)
+    del recipes_dict["ingredient-name[]"]
+    del recipes_dict["ingredient-qty[]"]
+    del recipes_dict["ingredient-units[]"]
+    del recipes_dict["steps"]
+    recipes_dict["ingredients"]=items
+    recipes_dict["allergens"]=form_allergens
+    recipes_dict["steps"]=form_steps
+    recipes.insert_one(recipes_dict)
+    return render_template('add_recipe.html')    
 
 
 if __name__=="__main__":
