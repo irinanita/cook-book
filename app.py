c2c655fb645716251ec641a9346206ca9d32103c
Irina Nita <irinanita.im@gmail.com>
2019-02-16 13:31:21 +0000

    Added user registration feature

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
 import os
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask,render_template,redirect,request,url_for,session,flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import datetime
 from bson.objectid import ObjectId
 
 app=Flask(__name__)
+app.secret_key = "thisaintnosecret"
 app.config["MONGO_DBNAME"]="cook_book"
 app.config["MONGO_URI"]="mongodb://admin:ceckbrb05@ds121295.mlab.com:21295/cook_book"
 
 mongo=PyMongo(app)
 
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == ['POST']:
+        session.pop('user',None)
+        if request.form['password']=='password':
+            session['user']=request.form['user']
+            return render_template('index.html')    
+    return render_template('login.html')
 
-@app.route('/')
+@app.route('/new_user')
+def new_user():
+    return render_template('new_user.html')  
+
+@app.route('/new_user/register', methods=['GET','POST'])
+def register():
+    user={}
+    users=mongo.db.users
+    username=request.form['user']
+    if users.find_one({"user":username}):
+        print('existing user, pick another username')
+        message="User already exists, please pick another username"
+        return render_template('new_user.html',message=message)
+    else:    
+        user["user"]=username
+        user["password"]=request.form['password']
+        users.insert_one(user)
+        print(user,'created')
+        message="User created successfully"
+    return render_template('login.html',message=message)  
+
+    
+
+@app.route('/home')
 def index():
     recipes=mongo.db.recipes.find().sort('_id', -1).limit(4) #return the latest 4 recipes
     return render_template('index.html',recipes=recipes)
@@ -39,13 +71,10 @@ def recipes():
     else:
         findParams = {}
    
-   
     if "keyword" in request.args:
         keyword = request.args.get('keyword')
         mongo.db.recipes.create_index([('$**','text')])
         findParams['$text'] = { '$search': keyword  }
-        
-    print(findParams)
    
     sortField = "_id"
     sortOrder = -1
